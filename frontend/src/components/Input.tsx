02c691c3ea63f6c01f7b798552ed9e33698d9e4a
Bape <154688709+BapeCode@users.noreply.github.com>
2026-03-17 09:29:46 +0100

    Add Button Components

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -8,11 +8,11 @@ interface InputProps {
 export default function Input({label, type = "text", placeholder}: InputProps) {
     return (
         <div className="flex flex-col items-start gap-2 w-full">
-            <label className="text-sm font-medium text-text-1/40 uppercase tracking-widest">{label}</label>
+            <label className="text-xs font-light text-text-1/40 uppercase tracking-widest">{label}</label>
             <input
                 type={type}
                 placeholder={placeholder}
-                className="w-full px-3 py-4 shadow-sm border-border focus:outline-none focus:ring-1 focus:ring-border-gold focus:ring-offset-2" />
+                className="w-full px-2.5 py-3 shadow-sm border-border focus:outline-none focus:ring-1 focus:ring-border-gold focus:ring-offset-2" />
         </div>
     )
 }
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,18 +1,24 @@
 import Section from "../components/Section.tsx";
 import Input from "../components/Input.tsx";
+import Button from "../components/Button.tsx";
 
 export default function Login() {
     return (
         <Section>
             <div className="flex justify-center items-center gap-6 h-screen">
-                <div className="bg-card border-border rounded-xs shadow-sm p-6">
+                <div className="bg-card border-border rounded-xs shadow-sm p-6 w-1/3">
                     <div className="flex flex-col items-start justify-center gap-1">
                         <h1 className="tracking-widest font-light text-text-1 font-serif text-lg">Connexion</h1>
                         <p className="font-extralight text-text-1/40 font-mono text-sm">Entrez vos identifiants pour accéder à votre compte</p>
                     </div>
 
-                    <form className="flex flex-col items-start justify-center mt-8 w-full">
+                    <form className="flex flex-col items-start justify-center mt-8 w-full gap-4">
                         <Input label="Adresse mail" placeholder="votre@email.fr" type="email"/>
+                        <Input label="Mot de passe" placeholder="****" type="password"/>
+
+                        <Button type="submit" className="w-full">
+                            <p className="text-text-1 uppercase text-sm font-medium">Se connecter</p>
+                        </Button>
                     </form>
                 </div>
             </div>

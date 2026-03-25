6b531c8d3e11a91cd9c3ddde04f31b458560b9da
David-MED-s <davidsanchez38550@gmail.com>
2026-03-25 11:15:33 +0100

    Fix Footer Features.tsx & Premium.tsx

--- frontend/src/pages/Premium.tsx
+++ frontend/src/pages/Premium.tsx
@@ -6,7 +6,7 @@ export default function Premium() {
   return (
     <>
       <Header />
-      <div className="mx-auto max-w-7xl px-6 py-16">
+      <div className="mx-auto max-w-7xl px-6 py-16 min-h-screen">
         <h1 className="text-4xl font-bold mb-8">Devenir Premium</h1>
         <p className="text-lg text-muted-foreground mb-6">
           En tant que membre Premium, tu bénéficieras de fonctionnalités
@@ -20,57 +20,6 @@ export default function Premium() {
           En devenant Premium, tu aides GLINT à continuer à créer un réseau
           social. Merci de faire partie de cette aventure avec nous !
         </p>
-        <div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, fuga
-            ullam sint corporis aliquam exercitationem. Ab qui sed modi facere
-            architecto assumenda necessitatibus provident quasi ut distinctio
-            sapiente quidem enim, amet unde illum error ducimus repellat
-            voluptas vel eveniet. Ad modi necessitatibus inventore sit ullam
-            ipsa, rerum dignissimos officiis. Ad veniam, illum omnis fuga ullam
-            quis voluptatibus repellat assumenda dicta voluptates dolorum dolore
-            facere consectetur quidem enim nisi! Non distinctio eum totam nihil
-            dolorum vitae consectetur quos nemo, quas tempore, obcaecati
-            accusantium sequi voluptatibus eos natus delectus. Explicabo,
-            suscipit incidunt ducimus dolore deleniti ut cupiditate
-            exercitationem blanditiis maiores, consequuntur ex nihil veniam quae
-            soluta recusandae numquam non omnis, excepturi molestiae modi animi
-            ea saepe eum tenetur. Maiores nostrum asperiores dicta reprehenderit
-            omnis non, ipsum recusandae neque sunt enim delectus ullam voluptate
-            ad autem temporibus aliquam voluptatibus obcaecati quo odio!
-            Laborum, fugiat ipsum, vel mollitia rerum voluptatem odit eos,
-            repudiandae expedita eaque sit. Iusto quaerat recusandae, possimus
-            provident amet aspernatur? Quaerat quos rem modi doloribus sequi
-            corrupti laboriosam eos error nisi maiores totam neque, vel officia.
-            Asperiores laboriosam impedit iure quis doloribus porro, officiis
-            pariatur accusamus sapiente error fugit quod alias delectus,
-            voluptatem nam quidem at earum tenetur officia assumenda. Sapiente.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, fuga
-            ullam sint corporis aliquam exercitationem. Ab qui sed modi facere
-            architecto assumenda necessitatibus provident quasi ut distinctio
-            sapiente quidem enim, amet unde illum error ducimus repellat
-            voluptas vel eveniet. Ad modi necessitatibus inventore sit ullam
-            ipsa, rerum dignissimos officiis. Ad veniam, illum omnis fuga ullam
-            quis voluptatibus repellat assumenda dicta voluptates dolorum dolore
-            facere consectetur quidem enim nisi! Non distinctio eum totam nihil
-            dolorum vitae consectetur quos nemo, quas tempore, obcaecati
-            accusantium sequi voluptatibus eos natus delectus. Explicabo,
-            suscipit incidunt ducimus dolore deleniti ut cupiditate
-            exercitationem blanditiis maiores, consequuntur ex nihil veniam quae
-            soluta recusandae numquam non omnis, excepturi molestiae modi animi
-            ea saepe eum tenetur. Maiores nostrum asperiores dicta reprehenderit
-            omnis non, ipsum recusandae neque sunt enim delectus ullam voluptate
-            ad autem temporibus aliquam voluptatibus obcaecati quo odio!
-            Laborum, fugiat ipsum, vel mollitia rerum voluptatem odit eos,
-            repudiandae expedita eaque sit. Iusto quaerat recusandae, possimus
-            provident amet aspernatur? Quaerat quos rem modi doloribus sequi
-            corrupti laboriosam eos error nisi maiores totam neque, vel officia.
-            Asperiores laboriosam impedit iure quis doloribus porro, officiis
-            pariatur accusamus sapiente error fugit quod alias delectus,
-            voluptatem nam quidem at earum tenetur officia assumenda. Sapiente.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, fuga
-          </p>
-        </div>
       </div>
       <Footer />;
     </>

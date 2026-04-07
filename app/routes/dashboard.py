3d365124a61439bdc0daf04814c2d57f78200bfd
Bape <154688709+BapeCode@users.noreply.github.com>
2026-04-07 12:57:45 +0200

    Refactor

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint
 
 from app.controllers.DashboardControllers import DashboardController
 from app.decorators.auth import login_required
@@ -9,21 +9,28 @@ dashboard_bp = Blueprint("dashboard", __name__)
 @dashboard_bp.route("/dashboard", methods=["GET", "POST"])
 @login_required
 def index(current_user):
-    tab = request.args.get("tab", "fill")
-
-    if tab == "fill":
-        return DashboardController.fill(current_user)
-
-    return DashboardController.index(current_user, tab)
+    return DashboardController.index(current_user)
 
 
 @dashboard_bp.route("/dashboard/profile", methods=["GET", "POST"])
 @login_required
 def profile(current_user):
-    return DashboardController.profil(current_user)
+    return DashboardController.profile(current_user)
 
 
-@dashboard_bp.route("dashboard/profile_edit", methods=["GET", "POST"])
+@dashboard_bp.route("/dashboard/<int:user_id>/profile", methods=["GET", "POST"])
+@login_required
+def other_profile(current_user, user_id):
+    return DashboardController.profile(current_user)
+
+
+@dashboard_bp.route("/dashboard/profile_edit", methods=["GET", "POST"])
 @login_required
 def profile_edit(current_user):
     return DashboardController.profil_edit(current_user)
+
+
+@dashboard_bp.route("/dashboard/messages", methods=["GET", "POST"])
+@login_required
+def messages(current_user):
+    return DashboardController.messages(current_user)

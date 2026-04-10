f4043de8195e01e93670004698973c16e350e63f
Dshellz <davidsanchez38550@gmail.com>
2026-04-10 12:36:56 +0200

    suppresion de la variable inutilisé

--- app/controllers/DashboardControllers.py
+++ app/controllers/DashboardControllers.py
@@ -159,7 +159,7 @@ class DashboardController:
 
     @staticmethod
     def messages(current_user):
-        posts = PostControllers.get_post(current_user)
+        PostControllers.get_post(current_user)
         all_users = User.query.all()
 
         return render_template(
